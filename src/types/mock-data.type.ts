1ce405108655024051aa9690d428e685bbc1e9b2
Anastasia <dzheyn.anna@mail.ru>
2023-05-12 23:57:09 +0400

    add mock server data

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,12 @@
+export type MockData = {
+  titles: string[],
+  descriptions: string[],
+  previewImages: string[],
+  images: string[],
+  users: string[],
+  emails: string[],
+  avatars: string[],
+  cities: string[],
+  types: string[],
+  goods: string[]
+}
