02d4f0b95c851a854b061899167c018c773c8806
Anastasia <dzheyn.anna@mail.ru>
2023-05-28 01:20:49 +0400

    update offer service wip

--- src/core/modules/offer/offer-service.interface.ts
+++ src/core/modules/offer/offer-service.interface.ts
@@ -1,8 +1,18 @@
 import { DocumentType } from '@typegoose/typegoose';
 import CreateOfferDto from './dto/create-offer.js';
 import { OfferEntity } from './offer.entity';
+import UpdateOfferDto from './dto/update-offer.js';
 
 export interface OfferServiceInterface {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
+  updateById(offerId: string, dto:UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(count?:number): Promise<DocumentType<OfferEntity>[]>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findPremium(count: number): Promise<DocumentType<OfferEntity>[]>;
+  findFavorite():Promise<DocumentType<OfferEntity>[]>;
+  updateFavoriteStatus(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateRating(offerId: string): Promise<number | null >;
+  exists(documentId: string): Promise<boolean>;
 }
--- src/core/modules/offer/offer.service.ts
+++ src/core/modules/offer/offer.service.ts
@@ -2,16 +2,19 @@ import { inject, injectable } from 'inversify';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { OfferServiceInterface } from './offer-service.interface.js';
-import { AppComponent, LoggerInfoMessage } from '../../../utils/constants.js';
 import { LoggerInterface } from '../../../types/core/logger.interface.js';
 import CreateOfferDto from './dto/create-offer.js';
+import { AppComponent } from '../../../types/app-component.enum.js';
+import { LoggerInfoMessage } from '../../logger/logger.constants.js';
+import { DEFAULT_AMOUNT } from './offer.constants.js';
+import UpdateOfferDto from './dto/update-offer.js';
 
 @injectable()
 export default class OfferService implements OfferServiceInterface {
   constructor(
     @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
     @inject(AppComponent.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>
-  ) {}
+  ) { }
 
   public async create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>> {
     const result = await this.offerModel.create(dto);
@@ -20,7 +23,36 @@ export default class OfferService implements OfferServiceInterface {
     return result;
   }
 
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, { new: true })
+      .populate(['userId', 'locationId']).exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async find(count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_AMOUNT;
+    return this.offerModel.find({}, {}, { limit });
+  }
+
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel.findById(offerId).populate(['userId', 'locationId']).exec();
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({ _id: documentId })) !== null;
   }
 }
